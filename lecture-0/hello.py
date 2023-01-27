5956fbde0526f58ee554e69d9e0a3bc09358ac64
Hkiondo <ngina79@gmail.com>
2023-01-27 17:34:02 +0300

    WIP:Introduction to python

---
+++ lecture-0/hello.py
@@ -0,0 +1,19 @@
+print("Hello, world!")
+
+
+#naming conventions in python
+#1.CamelCase
+#2.cpascalCase
+#3.snake_case
+
+NAME=""
+NUMBERS=0
+def another_hello_world(string:str):
+    argument_passed=string
+    NUMBERS=20
+    print(argument_passed, NUMBERS)
+
+if __name__=="__main__":
+    another_hello_world("Function called")
+
+#variables
